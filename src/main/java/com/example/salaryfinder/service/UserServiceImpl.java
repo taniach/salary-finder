e8d794f71e9d7efb6bb1b44cf1cd7c4362ab2092
Tania Chattopadhyay <taniach@fb.com>
2022-12-07 00:25:27 +0800

    Remove unused import

--- src/main/java/com/example/salaryfinder/service/UserServiceImpl.java
+++ src/main/java/com/example/salaryfinder/service/UserServiceImpl.java
@@ -4,13 +4,12 @@ import com.example.salaryfinder.domain.CustomSortOrder;
 import com.example.salaryfinder.domain.User;
 import com.example.salaryfinder.helper.OffsetLimitPageRequest;
 import com.example.salaryfinder.repo.UserRepository;
-import com.opencsv.bean.CsvToBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
