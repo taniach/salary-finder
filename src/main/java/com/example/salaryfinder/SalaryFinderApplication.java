6c43146165ca7725412785b9961e1d601b02998e
Tania Chattopadhyay <taniach@fb.com>
2022-12-06 21:30:19 +0800

    Clean up code

--- src/main/java/com/example/salaryfinder/SalaryFinderApplication.java
+++ src/main/java/com/example/salaryfinder/SalaryFinderApplication.java
@@ -2,7 +2,6 @@ package com.example.salaryfinder;
 
 import com.example.salaryfinder.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/example/salaryfinder/repo/UserRepository.java
+++ src/main/java/com/example/salaryfinder/repo/UserRepository.java
@@ -12,6 +12,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, String> {
     @Query("SELECT u FROM User u WHERE u.salary >= :min AND u.salary <= :max")
     Page<User> findAllUsersWithinLimit(@Param("min") double min, @Param("max") double max, Pageable pageable);
-
     Optional<User> findByNameIgnoreCase(String name);
 }
